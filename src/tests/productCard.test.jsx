2a420c566676095760ce8a226a614c31f7f8b9b5
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-03 12:11:15 -0300

    feat: add tests for `productCard`

---
+++ src/tests/productCard.test.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+import ProductCard from '../components/productCard/ProductCard';
+
+describe('Test ProductCard component', () => {
+  it('Should render the correct information from products', () => {
+    renderWithRouter(
+      <ProductCard
+        id="MLB2660706077"
+        title="Apple iPhone 13 Pro Max (128 Gb) - Grafite"
+        thumbnail="http://http2.mlstatic.com/D_879647-MLA47781235699_102021-I.jpg"
+        price={7949.99}
+      />,
+    );
+    const productTitle = screen.getByText('Apple iPhone 13 Pro Max (128 Gb) - Grafite');
+    expect(productTitle).toBeInTheDocument();
+    expect(productTitle.innerHTML).toEqual('Apple iPhone 13 Pro Max (128 Gb) - Grafite');
+
+    const productImage = screen.getByRole('img');
+    expect(productImage).toBeInTheDocument();
+    expect(productImage).toHaveAttribute('src');
+
+    const productPrice = screen.getByText('7949.99');
+    expect(productPrice).toBeInTheDocument();
+  });
+});
