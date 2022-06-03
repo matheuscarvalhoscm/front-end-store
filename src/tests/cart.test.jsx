b6e17f527c06e235f91b3b50d1e52b9dedb83608
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-03 12:13:22 -0300

    feat: add tests for cart page

---
+++ src/tests/cart.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './utils/renderWithRouter';
+
+import Cart from '../pages/Cart';
+
+describe('Test Cart page', () => {
+  it('Should render a message when the cart is empty', () => {
+    renderWithRouter(<Cart />);
+    const emptyCartMessage = screen.getByRole('heading', { level: 2 });
+
+    expect(emptyCartMessage.innerHTML).toEqual('Seu carrinho está vazio');
+  });
+});
