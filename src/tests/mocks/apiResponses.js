8183e87f0ab6a9c9707afa5fb7c210f728c179b6
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-03 12:03:41 -0300

    feat: add api mock responses for tests

---
+++ src/tests/mocks/apiResponses.js
@@ -0,0 +1,41 @@
+export const categoriesResponse = [
+  {
+    id: 'MLB5672',
+    name: 'Acessórios para Veículos',
+  },
+  {
+    id: 'MLB271599',
+    name: 'Agro',
+  },
+  {
+    id: 'MLB1403',
+    name: 'Alimentos e Bebidas',
+  },
+  {
+    id: 'MLB1071',
+    name: 'Animais',
+  },
+  {
+    id: 'MLB1367',
+    name: 'Antiguidades e Coleções',
+  },
+  {
+    id: 'MLB1368',
+    name: 'Arte, Papelaria e Armarinho',
+  },
+];
+
+export const mockedResponse = [
+  {
+    id: 'MLB1462812320',
+    site_id: 'MLB',
+    title: 'Mouse Logitech M90 Preto',
+    seller: {
+      id: '205904314',
+      permalink: 'http://perfil.mercadolivre.com.br/CYBERPOINTNET-ES',
+      registration_date: '2016-02-13T08:14:36.000-04:00',
+      car_dealer: false,
+      real_estate_agency: false,
+    },
+  },
+];
