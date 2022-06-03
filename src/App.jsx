06cf063992c1d7845e029a7e5cb1683fdce9e2c7
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-03 12:15:20 -0300

    feat!: update `App.js`
    add all routes and provider

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import AppProvider from './context/AppProvider';
+
+import Main from './pages/Main';
+import Cart from './pages/Cart';
+import ProductDetails from './pages/ProductDetails';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <div className="App">
+      <AppProvider>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/cart" component={Cart} />
+          <Route exact path="/details/:title" component={ProductDetails} />
+          <Route exact path="/checkout" component={Checkout} />
+        </Switch>
+      </AppProvider>
+    </div>
+  );
+}
+
+export default App;
