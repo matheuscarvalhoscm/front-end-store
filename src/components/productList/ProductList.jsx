ba28a904069c53f2cffa752cc92eef6762d13a88
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-06-29 15:25:51 -0300

    style: `productList` style

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -5,6 +5,7 @@ import cartIcon from '../../icons/shopping-cart.svg';
 import { getProductsByCategoryAndQuery } from '../../services/api';
 import Categories from '../categories/Categories';
 import ProductCard from '../productCard/ProductCard';
+import './productList.css';
 
 function ProductList() {
   const { filterCategory, cart } = useContext(AppContext);
@@ -19,37 +20,45 @@ function ProductList() {
   const cartQuantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
 
   return (
-    <main>
-      <input
-        type="text"
-        name="search-input"
-        placeholder="Digite algum termo de pesquisa"
-        onChange={({ target }) => setSearchInputText(target.value)}
-        value={searchInputText}
-      />
-      <button
-        type="button"
-        onClick={() => handleSearch(filterCategory, searchInputText)}
-      >
-        Buscar
-      </button>
-      <Link to="/cart">
-        <img name="cart" src={cartIcon} alt="Shopping cart icon" />
-        <span>{cartQuantity}</span>
-      </Link>
+    <main className="product-list">
+      <header>
+        <h2>
+          front-end
+          <span>store</span>
+        </h2>
+        <input
+          type="text"
+          name="search-input"
+          placeholder="Digite algum termo de pesquisa"
+          onChange={({ target }) => setSearchInputText(target.value)}
+          value={searchInputText}
+        />
+        <button
+          type="button"
+          onClick={() => handleSearch(filterCategory, searchInputText)}
+        >
+          Buscar
+        </button>
+        <Link to="/cart">
+          <img name="cart" src={cartIcon} alt="Shopping cart icon" />
+          <span>{cartQuantity}</span>
+        </Link>
+      </header>
       <Categories />
-      {
-        products.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
-          : products.map((product) => (
-            <ProductCard
-              id={product.id}
-              title={product.title}
-              thumbnail={product.thumbnail}
-              price={product.price}
-              key={product.id}
-            />
-          ))
-      }
+      <section className="product-card-section">
+        {
+          products.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+            : products.map((product) => (
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                thumbnail={product.thumbnail}
+                price={product.price}
+                key={product.id}
+              />
+            ))
+        }
+      </section>
     </main>
   );
 }
