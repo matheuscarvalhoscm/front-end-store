46cdc2fe3bcfd20ebf218e8f7cad1ef5c28b4989
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-05 14:56:03 -0300

    refactor: refactor `ProductList` style

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -3,8 +3,6 @@ import ScrollReveal from 'scrollreveal';
 import AppContext from '../../context/AppContext';
 import './productList.css';
 
-import Header from '../Header/Header';
-import Categories from '../categories/Categories';
 import ProductCard from '../productCard/ProductCard';
 
 function ProductList() {
@@ -13,24 +11,20 @@ function ProductList() {
   useEffect(() => ScrollReveal().reveal('.product-card'));
 
   return (
-    <main className="product-list">
-      <Header path="/" />
-      <Categories />
-      <section className="product-card-section">
-        {
-          products.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
-            : products.map((product) => (
-              <ProductCard
-                id={product.id}
-                title={product.title}
-                thumbnail={product.thumbnail}
-                price={product.price}
-                key={product.id}
-              />
-            ))
-        }
-      </section>
-    </main>
+    <section className="product-card-section">
+      {
+        products.length < 1 ? <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+          : products.map((product) => (
+            <ProductCard
+              id={product.id}
+              title={product.title}
+              thumbnail={product.thumbnail}
+              price={product.price}
+              key={product.id}
+            />
+          ))
+      }
+    </section>
   );
 }
 
