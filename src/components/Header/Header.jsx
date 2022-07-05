c60d22d5ffdc3c0b846a2f7a79cd1ba2d4bcdcd4
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-05 11:51:42 -0300

    feat: create `Header` component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AppContext from '../../context/AppContext';
+import { getProductsByCategoryAndQuery } from '../../services/api';
+import cartIcon from '../../icons/shopping-cart.svg';
+
+function Header({ path }) {
+  const { filterCategory, setProducts, cart } = useContext(AppContext);
+  const [searchInputText, setSearchInputText] = useState('');
+
+  const handleSearch = async (category, query) => {
+    const { data: { results } } = await getProductsByCategoryAndQuery(category, query);
+    setProducts(results);
+  };
+
+  const cartQuantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
+
+  if (path !== '/') {
+    return (
+      <header>
+        <h2>
+          front-end
+          <span>store</span>
+        </h2>
+        <Link to="/cart">
+          <img name="cart" src={cartIcon} alt="Shopping cart icon" />
+          <span>{cartQuantity}</span>
+        </Link>
+      </header>
+    );
+  }
+
+  return (
+    <header>
+      <h2>
+        front-end
+        <span>store</span>
+      </h2>
+      <input
+        type="text"
+        name="search-input"
+        placeholder="Digite algum termo de pesquisa"
+        onChange={({ target }) => setSearchInputText(target.value)}
+        value={searchInputText}
+      />
+      <button
+        type="button"
+        onClick={() => handleSearch(filterCategory, searchInputText)}
+      >
+        Buscar
+      </button>
+      <Link to="/cart">
+        <img name="cart" src={cartIcon} alt="Shopping cart icon" />
+        <span>{cartQuantity}</span>
+      </Link>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  path: '/',
+};
+
+Header.propTypes = {
+  path: PropTypes.string,
+};
+
+export default Header;
