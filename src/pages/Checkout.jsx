252fc93cb682249cb3a4e85c061413384b148033
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-05 11:19:40 -0300

    feat!: update checkou form
    still in progress

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import AppContext from '../context/AppContext';
 import getStates from '../services/statesApi';
+import '../styles/checkout/checkout.css';
 
 import boleto from '../icons/boleto.svg';
 import masterCard from '../icons/mastercard.svg';
@@ -32,9 +33,9 @@ function Checkout() {
   }
 
   return (
-    <main>
+    <main className="checkout-main-container">
       <BackPageLink pathName="/cart" />
-      <section>
+      <section className="checkout-container">
         <h2>Revise seu pedido</h2>
         <ul>
           {
@@ -49,31 +50,92 @@ function Checkout() {
         </ul>
         <p>{`Total: R$ ${totalValue}`}</p>
       </section>
-      <form>
-        <h2>Informações do comprador</h2>
-        <input type="text" placeholder="Nome completo" />
-        <input type="number" placeholder="CPF" />
-        <input type="email" placeholder="Email" />
-        <input type="tel" placeholder="Telefone" />
-        <input type="number" placeholder="CEP" />
-        <input type="text" placeholder="Endereço" />
-        <input type="text" placeholder="Complemento" />
-        <input type="number" placeholder="Número" />
-        <input type="text" placeholder="Cidade" />
-        <select>
-          {
-            states.map(({ id, sigla, nome }) => <option key={id} value={sigla}>{nome}</option>)
-          }
-        </select>
-        <section>
-          <h2>Método de pagamento</h2>
-          <input type="radio" value="boleto" name="payment-method" />
-          <img src={boleto} alt="Pagamento por boleto" />
-          <input type="radio" value="mastercard" name="payment-method" />
-          <img src={masterCard} alt="Pagamento cartão de crédito Mastercard" width="50px" />
-          <input type="radio" value="visa" name="payment-method" />
-          <img src={visa} alt="Pagamento cartão de crédito Visa" width="50px" />
-        </section>
+      <form className="checkout-form">
+        <fieldset className="checkout-customer-info">
+          <legend>Informações do comprador</legend>
+          <label htmlFor="full-name-input">Nome completo:</label>
+          <input type="text" placeholder="Nome completo" id="full-name-input" />
+
+          <label htmlFor="cpf-number-input">CPF:</label>
+          <input type="number" placeholder="CPF" id="cpf-number" />
+
+          <label htmlFor="email-input">Email:</label>
+          <input type="email" placeholder="Email" id="email-input" />
+
+          <label htmlFor="phone-number-input">Telefone:</label>
+          <input type="tel" placeholder="Telefone" id="phone-number-input" />
+
+          <label htmlFor="zip-code-input">CEP:</label>
+          <input type="number" placeholder="CEP" id="zip-code-input" />
+
+          <label htmlFor="address-input">Endereço:</label>
+          <input type="text" placeholder="Endereço" id="address-input" />
+
+          <label htmlFor="address-complement-input">Complemento:</label>
+          <input type="text" placeholder="Complemento" id="address-complement-input" />
+
+          <label htmlFor="address-number-input">Número:</label>
+          <input type="number" placeholder="Número" id="address-number-input" />
+
+          <label htmlFor="city-name-input">Cidade:</label>
+          <input type="text" placeholder="Cidade" id="city-name-input" />
+
+          <label htmlFor="state-input">Estado:</label>
+          <select>
+            {
+              states.map(({ id, sigla, nome }) => (
+                <option
+                  key={id}
+                  value={sigla}
+                  id="state-input"
+                >
+                  {nome}
+                </option>
+              ))
+            }
+          </select>
+        </fieldset>
+        <fieldset className="checkout-paying-methods">
+          <legend>Método de pagamento</legend>
+          <label htmlFor="ticket-input">
+            Boleto
+            <input
+              type="radio"
+              value="boleto"
+              name="payment-method"
+              id="ticket-input"
+            />
+            <img src={boleto} alt="Pagamento por boleto" />
+          </label>
+          <label htmlFor="mastercard-input">
+            Mastercard
+            <input
+              type="radio"
+              value="mastercard"
+              name="payment-method"
+              id="mastercard-input"
+            />
+            <img
+              src={masterCard}
+              alt="Pagamento cartão de crédito Mastercard"
+              width="50px"
+            />
+          </label>
+          <label htmlFor="visa-input">
+            Visa
+            <input
+              type="radio"
+              value="visa"
+              name="payment-method"
+              id="visa-input"
+            />
+            <img
+              src={visa}
+              alt="Pagamento cartão de crédito Visa"
+              width="50px"
+            />
+          </label>
+        </fieldset>
       </form>
       <button type="button">
         Comprar
