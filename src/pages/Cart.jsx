d7a2672cd09c2dbb688c92d0c93e8c05a4a496a4
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-05 11:21:29 -0300

    style: implement cart page style

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import AppContext from '../context/AppContext';
+import '../styles/cart/cart.css';
 
 import BackPageLink from '../components/backPageLink/backPageLink';
 
@@ -28,7 +29,8 @@ function Cart() {
     setCart(updateProduct);
   };
 
-  const totalValue = cart.reduce((acc, curr) => acc + (curr.quantity * curr.price), 0);
+  const totalValue = cart.reduce((acc, curr) => acc + (curr.quantity * curr.price), 0).toFixed(2);
+  const totalQuantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
 
   if (cart.length < 1) {
     return (
@@ -41,36 +43,40 @@ function Cart() {
   }
 
   return (
-    <main>
+    <main className="cart">
       <BackPageLink pathName="/" />
-      <h1>Carrinho de Compras</h1>
-      <ul>
+      <h1>{`Carrinho de Compras (${totalQuantity})`}</h1>
+      <section className="cart-items-section">
         {
           cart.map((product) => (
-            <li key={product.title}>
-              <img src={product.thumbnail} alt={product.id} />
-              <p>{product.title}</p>
-              <button
-                type="button"
-                value="-"
-                onClick={() => decreaseQuantity(product.id)}
-              >
-                -
-              </button>
-              <p>{product.quantity}</p>
-              <button
-                type="button"
-                value="+"
-                onClick={() => increaseQuantity(product.id)}
-              >
-                +
-              </button>
-              <p>{`R$ ${product.price * product.quantity}`}</p>
-            </li>
+            <div className="cart-items-inner" key={product.title}>
+              <div className="cart-items-details">
+                <img src={product.thumbnail} alt={product.id} />
+                <p>{product.title}</p>
+              </div>
+              <div className="cart-items-buttons">
+                <button
+                  type="button"
+                  value="-"
+                  onClick={() => decreaseQuantity(product.id)}
+                >
+                  -
+                </button>
+                <p>{product.quantity}</p>
+                <button
+                  type="button"
+                  value="+"
+                  onClick={() => increaseQuantity(product.id)}
+                >
+                  +
+                </button>
+                <p>{`R$ ${(product.price * product.quantity)}`}</p>
+              </div>
+            </div>
           ))
         }
-      </ul>
-      <p>{`Valor total da compra: R$ ${totalValue}`}</p>
+      </section>
+      <h2>{`Valor total da compra: R$ ${totalValue}`}</h2>
       <button type="button" onClick={() => history.push('/checkout')}>
         Finalizar compra
       </button>
