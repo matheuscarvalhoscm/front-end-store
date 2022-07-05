4699ddf01dc58653f1485b324801378415047585
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-05 14:56:46 -0300

    refactor: use components on `Main` page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+
+import Header from '../components/Header/Header';
+import Categories from '../components/categories/Categories';
 import ProductList from '../components/productList/ProductList';
 
 function Main() {
   return (
-    <ProductList />
+    <main className="product-list">
+      <Header path="/" />
+      <Categories />
+      <ProductList />
+    </main>
   );
 }
 
