0af7002ccaafd15865b624423bf6e63e36f8f031
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-06 11:10:32 -0300

    feat: create `CustomerProducts` component

---
+++ src/components/CustomerProducts/CustomerProducts.jsx
@@ -0,0 +1,111 @@
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AppContext from '../../context/AppContext';
+
+function CustomerProducts({ path }) {
+  const history = useHistory();
+  const { cart, setCart } = useContext(AppContext);
+
+  const handleQuantity = (productId, event) => {
+    const { value } = event.target;
+    let updateProduct;
+    if (value === '+') {
+      updateProduct = cart.map((item) => (
+        item.id === productId
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      ));
+    } else {
+      updateProduct = cart.map((item) => (
+        item.id === productId && item.quantity > 1
+          ? { ...item, quantity: item.quantity - 1 }
+          : item
+      ));
+    }
+
+    setCart(updateProduct);
+  };
+
+  const totalValue = cart.reduce((acc, curr) => acc + (curr.quantity * curr.price), 0).toFixed(2);
+  const totalQuantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
+  const title = path === 'cart' ? `Carrinho de Compras (${totalQuantity})` : 'Revise seu pedido';
+
+  const renderCart = (product) => (
+    <div className="cart-items-buttons">
+      <button
+        type="button"
+        value="-"
+        onClick={(e) => handleQuantity(product.id, e)}
+      >
+        -
+      </button>
+      <p>{product.quantity}</p>
+      <button
+        type="button"
+        value="+"
+        onClick={(e) => handleQuantity(product.id, e)}
+      >
+        +
+      </button>
+      <p>{`R$ ${(product.price * product.quantity)}`}</p>
+    </div>
+  );
+
+  const renderCheckout = (product) => (
+    <div className="cart-items-buttons">
+      <p>{product.quantity}</p>
+      <p>{`R$ ${(product.price * product.quantity)}`}</p>
+    </div>
+  );
+
+  const renderButton = () => (
+    <button type="button" onClick={() => history.push('/checkout')}>
+      Finalizar compra
+    </button>
+  );
+
+  if (cart.length < 1) {
+    return (
+      <main>
+        <h1>Carrinho de Compras</h1>
+        <h2>Seu carrinho está vazio</h2>
+      </main>
+    );
+  }
+
+  return (
+    <main className="cart">
+      <h1>{title}</h1>
+      <section className="cart-items-section">
+        {
+          cart.map((product) => (
+            <div className="cart-items-inner" key={product.title}>
+              <div className="cart-items-details">
+                <img src={product.thumbnail} alt={product.id} />
+                <p>{product.title}</p>
+              </div>
+              {
+                path === 'cart' ? renderCart(product) : renderCheckout(product)
+              }
+            </div>
+          ))
+        }
+      </section>
+      <h2>{`Valor total da compra: R$ ${totalValue}`}</h2>
+      {
+        path === 'cart' ? renderButton() : null
+      }
+    </main>
+  );
+}
+
+CustomerProducts.defaultProps = {
+  path: '/',
+};
+
+CustomerProducts.propTypes = {
+  path: PropTypes.string,
+};
+
+export default CustomerProducts;
