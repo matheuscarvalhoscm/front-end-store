578e9774670a3b18e2abaa3a77a2afa90bedc8af
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-06 11:11:14 -0300

    refactor: use `CustomerProducts` at cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,86 +1,14 @@
-import React, { useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import AppContext from '../context/AppContext';
-import '../styles/cart/cart.css';
+import React from 'react';
 
-import BackPageLink from '../components/BackPageLink/BackPageLink';
+import Header from '../components/Header/Header';
+import CustomerProducts from '../components/CustomerProducts/CustomerProducts';
 
 function Cart() {
-  const history = useHistory();
-  const { cart, setCart } = useContext(AppContext);
-
-  const handleQuantity = (productId, event) => {
-    const { value } = event.target;
-    let updateProduct;
-    if (value === '+') {
-      updateProduct = cart.map((item) => (
-        item.id === productId
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      ));
-    } else {
-      updateProduct = cart.map((item) => (
-        item.id === productId && item.quantity > 1
-          ? { ...item, quantity: item.quantity - 1 }
-          : item
-      ));
-    }
-
-    setCart(updateProduct);
-  };
-
-  const totalValue = cart.reduce((acc, curr) => acc + (curr.quantity * curr.price), 0).toFixed(2);
-  const totalQuantity = cart.reduce((acc, curr) => acc + curr.quantity, 0);
-
-  if (cart.length < 1) {
-    return (
-      <main>
-        <BackPageLink pathName="/" />
-        <h1>Carrinho de Compras</h1>
-        <h2>Seu carrinho está vazio</h2>
-      </main>
-    );
-  }
-
   return (
-    <main className="cart">
-      <BackPageLink pathName="/" />
-      <h1>{`Carrinho de Compras (${totalQuantity})`}</h1>
-      <section className="cart-items-section">
-        {
-          cart.map((product) => (
-            <div className="cart-items-inner" key={product.title}>
-              <div className="cart-items-details">
-                <img src={product.thumbnail} alt={product.id} />
-                <p>{product.title}</p>
-              </div>
-              <div className="cart-items-buttons">
-                <button
-                  type="button"
-                  value="-"
-                  onClick={(e) => handleQuantity(product.id, e)}
-                >
-                  -
-                </button>
-                <p>{product.quantity}</p>
-                <button
-                  type="button"
-                  value="+"
-                  onClick={(e) => handleQuantity(product.id, e)}
-                >
-                  +
-                </button>
-                <p>{`R$ ${(product.price * product.quantity)}`}</p>
-              </div>
-            </div>
-          ))
-        }
-      </section>
-      <h2>{`Valor total da compra: R$ ${totalValue}`}</h2>
-      <button type="button" onClick={() => history.push('/checkout')}>
-        Finalizar compra
-      </button>
-    </main>
+    <div>
+      <Header path="cart" />
+      <CustomerProducts path="cart" />
+    </div>
   );
 }
 
