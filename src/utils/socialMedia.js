33c103305d019c3afbb84c84f07e2f57e88af241
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-07-06 12:25:59 -0300

    feat: creat social media info array

---
+++ src/utils/socialMedia.js
@@ -0,0 +1,23 @@
+import github from '../icons/github.png';
+import linkedin from '../icons/linkedin.png';
+import email from '../icons/email.png';
+
+const socialMedia = [
+  {
+    image: github,
+    alt: 'Checkout my GitHub page!',
+    link: 'https://github.com/matheuscarvalhoscm',
+  },
+  {
+    image: linkedin,
+    alt: 'Checkout my Linkedin page!',
+    link: 'https://www.linkedin.com/in/matheuscarvalhoscm/',
+  },
+  {
+    image: email,
+    alt: 'Send me an email!',
+    link: 'mailto:matheuscarvalhoscm@gmail.com',
+  },
+];
+
+export default socialMedia;
